4864a600bc73c3281c8085601d00c5121898f9cf
Sodeeq Olapade <solapade@seamfix.com>
2019-09-16 18:13:50 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mkcert = require("mkcert");
+const https = require("https");
+const fs = require("fs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
+app.use("/", (req, res) => {
+  res.json({ msg: "welcome" });
+});
+
+const options = {
+  key: fs.readFileSync("./keys/localhost+1-key.pem"),
+  cert: fs.readFileSync("./keys/localhost+1.pem")
+};
+
+// async function genCert() {
+//   try {
+//     const ca = await mkcert.createCA({
+//       organization: "Hello CA",
+//       countryCode: "NP",
+//       state: "Bagmati",
+//       locality: "Kathmandu",
+//       validityDays: 365
+//     });
+
+//     // console.log("KEY: ", ca.key);
+//     // console.log("CERT: ", ca.cert);
+
+//     const cert = await mkcert.createCert({
+//       domains: ["127.0.0.1", "localhost"],
+//       validityDays: 365,
+//       caKey: ca.key,
+//       caCert: ca.cert
+//     });
+
+//     const fullCertChain = `${cert.cert}\n${ca.cert}`;
+//     const opt = {};
+
+//     // console.log("key: ", cert.key);
+//     // console.log("cert: ", cert.cert);
+
+//     console.log(fullCertChain);
+//     https.createServer(fullCertChain, app).listen(3000, (req, res) => {
+//       console.log("app running on 3000");
+//     });
+//   } catch (error) {
+//     console.log("Error: ", error.message);
+//   }
+// }
+
+// genCert();
+
+https.createServer(options, app).listen(3000, (req, res) => {
+  console.log("app running on 3000");
+});
